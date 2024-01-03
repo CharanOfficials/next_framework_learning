6f55a2e1f272320e3a3252a4270547bbd5d03341
CharanOfficials <charanofficials@gmail.com>
2024-01-03 16:08:15 +0530

    Dynamic meta data and Script to load component specific external libraries

--- src/app/login/image/page.js
+++ src/app/login/image/page.js
@@ -1,15 +1,23 @@
+"use client";
 import React from "react";
+import Script from "next/script";
 import Image from "next/image";
-const Pap = () => {
+export default function Pap() {
   return (
     <div>
+      <Script src="/localtion.js" onLoad={console.log("Loaded")} />
       <Image
         src="https://images.pexels.com/photos/18340828/pexels-photo-18340828/free-photo-of-man-in-traditional-north-american-indigenous-clothing.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         width={500}
         height={500}
+        alt=""
       />
     </div>
   );
-};
+}
 
-export default Pap;
+// export const generateMetadata = () => {
+//   return {
+//     title: "Image",
+//   };
+// };
